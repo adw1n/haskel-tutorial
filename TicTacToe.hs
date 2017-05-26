c0d4f70eed74ce7c08e804764c80ec3078bdb807
adw1n <adw1n@users.noreply.github.com>
2017-05-26 12:46:21 +0200

    winner funct

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -15,13 +15,36 @@ data GameBoard = GameBoard {
   board:: [(Player, Player, Player)]
 } deriving (Show, Eq)
 
+testWinner:: Player -> GameBoard -> Bool
+testWinner player board'
+  | (head board_) == winningSeq = True
+  | (last board_) == winningSeq = True
+  | (board_!!1) == winningSeq = True
+  | diagonal1 == winningSeq = True
+  | diagonal2 == winningSeq = True
+  | (x1,y1,z1) == winningSeq = True
+  | (x2,y2,z2) == winningSeq = True
+  | (x3,y3,z3) == winningSeq = True
+  | otherwise = False
+  where board_ = (board board')
+        winningSeq = (player, player, player)
+        (x1,x2,x3) = board_!!0
+        (y1,y2,y3) = board_!!1
+        (z1,z2,z3) = board_!!2
+        diagonal1 = (x1,y2,z3)
+        diagonal2 = (x3,y2,z1)
 
--- TODO
+
+
 -- Take the board and decide whether there is a winner.
 -- @return: the winner (O or X) or NA if no1 has won the game yet
 -- also consider doing 'error "Invalid board state"' if the board is in an invalid state
 winner:: GameBoard -> Player
-winner board = NA
+winner board
+  | (testWinner O board) && (testWinner X board) = error "Invalid board state"
+  | (testWinner O board) = O
+  | (testWinner X board) = X
+  | otherwise = NA
 
 
 -- TODO
